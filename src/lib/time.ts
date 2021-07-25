be4c56f2770c70a351c54f1189e06839a6439626
Phoscur <phoscur@pheelgood.net>
2021-07-25 19:28:32 +0200

    Add sundown timing API helpers: time & sundown

---
+++ src/lib/time.ts
@@ -0,0 +1,20 @@
+import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc'
+import timezone from 'dayjs/plugin/timezone' // dependent on utc plugin
+import duration from 'dayjs/plugin/duration'
+import relativeTime from 'dayjs/plugin/relativeTime'
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
+dayjs.extend(duration)
+dayjs.extend(relativeTime)
+
+const { LOCATION_TIMEZONE } = process.env
+
+export function changeTimezoneLondonToLocal(time: Date|number) {
+  return dayjs.tz(time, "Europe/London").tz(LOCATION_TIMEZONE)
+}
+
+export function humanizeDuration(diff: number) {
+  return dayjs.duration(diff).humanize()
+}
