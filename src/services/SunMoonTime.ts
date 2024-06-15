4ac6b32546b9d2559e0691abb19025ff7ad3caf7
Phoscur <phoscur@pheelgood.net>
2024-06-15 15:17:28 +0200

    Add SunMoonTime service generate list of yearly alignments

---
+++ src/services/SunMoonTime.ts
@@ -0,0 +1,101 @@
+import SunCalc from 'suncalc'
+
+export class SunMoonTime {
+  constructor(
+    readonly latitude = parseFloat(`${process.env.LOCATION_LATITUDE}`),
+    readonly longitude = parseFloat(`${process.env.LOCATION_LONGITUDE}`),
+    readonly timeZone = `${process.env.LOCATION_TIMEZONE}`,
+    public today: Date = new Date()
+  ) {
+    this.resetToday(today)
+  }
+
+  get tomorrow() {
+    return this.addDay(this.today)
+  }
+
+  addDay(date: Date, days = 1, hour = 12) {
+    return new Date(date.getFullYear(), date.getMonth(), date.getDate() + days, hour, 0, 0)
+  }
+
+  resetToday(date = new Date()) {
+    this.today = date
+    this.today.setHours(12, 0, 0, 0)
+  }
+
+  getSunset(date = this.today): Date {
+    const { sunset } = SunCalc.getTimes(date, this.latitude, this.longitude)
+    return sunset
+  }
+
+  /** not every date has a moonset everywhere! */
+  getMoonset(date = this.today): Date | undefined {
+    const { set } = SunCalc.getMoonTimes(date, this.latitude, this.longitude)
+    return set
+  }
+
+  getAlignment(date = this.today) {
+    const sun = this.getSunset(date)
+    let moon = this.getMoonset(date)
+    let diff = Number.POSITIVE_INFINITY
+    if (moon) {
+      diff = sun.getTime() - moon.getTime()
+    }
+    // if the moonset was before the sunset, we need to check the moonset of the next day
+    if (diff > 0) {
+      const morrowset = this.getMoonset(this.addDay(date))
+      if (morrowset) {
+        diff = sun.getTime() - morrowset.getTime()
+        moon = morrowset
+      }
+    }
+    return {
+      sun,
+      moon,
+      minutes: Math.floor(diff / 1000 / 60),
+    }
+  }
+}
+
+if (require.main === module) {
+  const year = 2024
+  const maxHours = 4
+  const sunMoonTime = new SunMoonTime()
+  for (let month = 0; month < 12; month++) {
+    for (let day = 1; day < 32; day++) {
+      const date = new Date(year, month, day, 12)
+      if (date.getMonth() !== month) {
+        continue
+      }
+      const { sun, moon, minutes } = sunMoonTime.getAlignment(date)
+      if (!moon) {
+        continue
+      }
+      if (Math.abs(minutes) > 60 * maxHours) {
+        continue
+      }
+      const { azimuth } = SunCalc.getMoonPosition(sun, sunMoonTime.latitude, sunMoonTime.longitude)
+      const angle = azimuth * (180 / Math.PI) + 180 // azimuth needs to be converted from -180° to 180° to 0° to 360°
+      if (angle < 230) {
+        continue
+      }
+      const illumination = SunCalc.getMoonIllumination(sun).fraction
+      if (illumination < 0.05) {
+        //continue
+      }
+      console.log(
+        (sun.getDate() < 10 ? ' ' : '') +
+          sun.toLocaleString('de-DE', { timeZone: sunMoonTime.timeZone }),
+        '|',
+        illumination.toFixed(2),
+        '|',
+        (moon.getDate() < 10 ? ' ' : '') +
+          moon.toLocaleString('de-DE', { timeZone: sunMoonTime.timeZone }),
+        '|',
+        angle.toFixed(2) + '°',
+        '|',
+        -minutes
+      )
+    }
+  }
+}
