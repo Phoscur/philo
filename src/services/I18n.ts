f47437bf89c4de8a4bd1c069d0c7095b7410eafa
Phoscur <phoscur@pheelgood.net>
2024-09-05 20:16:05 +0200

    Update I18n: use @joist/di StaticToken

--- src/services/I18n.ts
+++ src/services/I18n.ts
@@ -1,7 +1,7 @@
-import { injectable, token } from '@joist/di';
+import { injectable, StaticToken } from '@joist/di';
 import { I18n, useTranslations } from '../i18n.js';
 
-export const InjectableI18n = token<I18n>('I18n', () => useTranslations());
+export const InjectableI18n = new StaticToken<I18n>('I18n', () => useTranslations());
 
 @injectable
 export class I18nService {
