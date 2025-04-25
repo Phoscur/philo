67d5064a2a1abf075f6ab1722b6791149b4b06e5
Phoscur <phoscur@pheelgood.net>
2025-04-25 18:15:50 +0200

    Move spawnPromise function to ffmpeg

--- src/lib/ffmpeg.ts
+++ src/lib/ffmpeg.ts
@@ -1,4 +1,69 @@
-import { spawnPromisePrependStdErr } from './spawn.js';
+import { spawn, SpawnOptions } from 'child_process';
+// can't unify this with spawnPromise... because ffmpeg abuses stderr to post progress
+export const spawnPromisePrependStdErr = (
+  command: string,
+  args?: Array<string>,
+  options?: SpawnOptions,
+  onData = (_frame: string, _fps: string) => {}
+) =>
+  new Promise<Buffer>((resolve, reject) => {
+    const childProcess = spawn(command, args ?? [], options ?? {});
+
+    let stdoutData = Buffer.alloc(0);
+    let stderrData = Buffer.alloc(0);
+
+    if (!childProcess.stdout) {
+      throw new Error(`No 'stdout' available on spawned process '${command}'`);
+    }
+
+    if (!childProcess.stderr) {
+      throw new Error(`No 'stderr' available on spawned process '${command}'`);
+    }
+
+    childProcess.once('error', (err: Error) => reject(err));
+
+    childProcess.stdout.on('data', (data: Buffer) => {
+      stdoutData = Buffer.concat([stdoutData, data]);
+      console.log('[ffmpeg]', data.toString());
+    });
+    childProcess.once('error', (err: Error) => {
+      console.error('CMD failed', command, err);
+      // TODO? reject(err)
+    });
+    childProcess.stderr.on('data', (data: Buffer) => {
+      stderrData = Buffer.concat([stderrData, data]);
+      const str = data.toString();
+      console.log('[ffmpeg err]', str);
+      const pattern = /frame=[ ]*([0-9]+) fps=(([0-9]*[.])?[0-9]+)/g;
+      const match = pattern.exec(str);
+      if (match) {
+        const [_, frame, fps] = match;
+        onData(frame, fps);
+      }
+    });
+    childProcess.stderr.once('error', (err: Error) => {
+      console.log(
+        'Command unsuccessful',
+        command,
+        args,
+        'Error:',
+        stderrData.length,
+        'Output:',
+        stdoutData.length,
+        'Warnings:',
+        stderrData.toString()
+      );
+      reject(err);
+    });
+
+    childProcess.stdout.on('close', () => {
+      if (stderrData.length > 0) {
+        return resolve(Buffer.concat([stderrData, stdoutData]));
+      }
+
+      return resolve(stdoutData);
+    });
+  });
 
 export interface StitchOptions {
   framerate?: number;
@@ -42,7 +107,8 @@ export async function stitchImages(
       ? []
       : [
           '-vf',
-          'crop=iw-216:ih-628,scale=1920:1200',
+          'crop=iw-216:ih-748,scale=1920:1080',
+          //'crop=iw-216:ih-628,scale=1920:1200',
           '-c:v',
           'libx264',
           // crf encoding
